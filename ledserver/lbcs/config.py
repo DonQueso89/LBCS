49fed539e7217a7d305b6a1b946700188f8557a6
Kees van Ekeren <kg.v.ekeren@gmail.com>
2020-06-20 18:26:29 +0200

    add

--- ledserver/lbcs/config.py
+++ ledserver/lbcs/config.py
@@ -1,7 +1,28 @@
+from dataclasses import dataclass, fields, asdict
 from configparser import ConfigParser
 
 
-def get(fname: str) -> ConfigParser:
+@dataclass
+class LBCSConfig:
+    port: str
+    rows: int
+    columns: int
+    debug: bool
+
+    def normalized(self):
+        return self.__class__(
+            **{f.name: f.type(getattr(self, f.name)) for f in fields(self)}
+        )
+
+    def pretty(self):
+        cfg_ = asdict(self)
+        return "\n".join(
+            f"{k}".ljust(max(len(x) for x in cfg_)) + ": " + f"{v}"
+            for k, v in cfg_.items()
+        )
+
+
+def get(fname: str) -> LBCSConfig:
     cfg = ConfigParser()
     cfg.read(fname)
-    return cfg["app"]
+    return LBCSConfig(**dict(cfg["app"])).normalized()
--- ledserver/lbcs/server.py
+++ ledserver/lbcs/server.py
@@ -1,4 +1,8 @@
+from typing import Dict
+from dataclasses import asdict
 import configparser
+import json
+import logging
 import os
 
 import tornado.ioloop
@@ -11,41 +15,68 @@ from lbcs import config
 DEFAULT_CONFIG = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), "example_config.ini"
 )
-
-
 define("config_file", help="absolute path to config file", default=DEFAULT_CONFIG)
+
+logger = logging.getLogger(__file__)
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    def get_ledstate(n: int) -> int:
-        return 1
+    def initialize(self, leds: Dict[int, int]):
+        self.leds = leds
 
 
 class AllStateHandler(BaseHandler):
     def get(self):
-        self.write("all state")
+        self.write(json.dumps(self.leds))
 
 
 class StateHandler(BaseHandler):
+    def get(self, lednumber):
+        state = "off"
+        if self.leds[int(lednumber)]:
+            state = "on"
+        self.write(f"led {lednumber} is switched {state}")
+
+    def post(self, lednumber, state):
+        state = int(state)
+        new_state = "off"
+        if state:
+            new_state = "on"
+
+        self.leds[int(lednumber)] = state
+        self.write(f"Turned led {lednumber} {new_state}")
+
+
+class DimensionsHandler(BaseHandler):
+    def initialize(self, rows, columns, **kwargs):
+        self.rows = rows
+        self.columns = columns
+
     def get(self):
-        self.write("single state")
+        self.write(json.dumps({"rows": self.rows, "columns": self.columns}))
 
 
 class LBCSServer(tornado.web.Application):
-    def __init__(self, cfg: configparser.ConfigParser):
+    def __init__(self, cfg: config.LBCSConfig, leds: Dict[int, int]):
         handlers = [
-            (r"/state/", AllStateHAndler),
-            (r"/state/([0-9]+)/", SingleStateHandler),
+            (r"/dimensions/", DimensionsHandler, asdict(cfg)),
+            (r"/state/([0-9]+)/([0-1]{1})/", StateHandler, {"leds": leds}),
+            (r"/state/([0-9]+)/", StateHandler, {"leds": leds}),
+            (r"/state/", AllStateHandler, {"leds": leds}),
         ]
-        super().__init__(self, handlers, debug=bool(cfg["debug"]))
+        super().__init__(handlers, debug=cfg.debug)
 
 
 def main():
-    options = tornado.options.parse_command_line()
-    print(options)
+    tornado.options.parse_command_line()
     cfg = config.get(options.config_file)
-    app = LBCSServer(cfg)
-    app.listen(int(cfg["port"]))
+    leds = {i: 0 for i in range(cfg.rows * cfg.columns)}
+
+    app = LBCSServer(cfg, leds)
+    app.listen(cfg.port)
+
+    logger.info(f"Parsed server config\n{cfg.pretty()}\n")
+    logger.info(f"Starting Little Bull Climbing Server server on port {cfg.port}")
     tornado.ioloop.IOLoop.current().start()
 
 
