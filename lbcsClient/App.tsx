baf52a274dec4ad7065cbba004581e9de0a27725
Kees van Ekeren <kg.v.ekeren@gmail.com>
2020-06-20 20:04:27 +0200

    basic selectable grid

--- lbcsClient/App.tsx
+++ lbcsClient/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+function Cell(props) {
+  const [selected, setSelected] = useState(0);
+  const cellStyle =   {
+    flex: 1,
+    backgroundColor: selected ? "red" : "skyBlue",
+    borderColor: "black", 
+    borderWidth: 1, 
+    borderRadius: 5
+  }
+  
+  
+  return <TouchableOpacity style={cellStyle} onPress={() => setSelected(prevState => !prevState)} ></TouchableOpacity>
+}
+
+function Row(props) {
+  const cells = Array(props.cols).fill(null).map(x => <Cell columns={props.cols}/>)  
+
+  return <View style={styles.row}>{cells}</View>
+}
+
+function Grid(props) {
+  const width = 100 / props.cols;
+  const rows = Array(props.rows).fill(null).map(x => <Row cols={props.cols}/>)  
+  return <View style={styles.container}>{rows}</View>
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Grid rows={12} cols={12}/>
     </View>
   );
 }
@@ -12,8 +42,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'stretch'
+  }
 });
