a3e83e1ba912625c02fd5f765595cffe69d05827
Kees van Ekeren <kg.v.ekeren@gmail.com>
2020-06-22 10:20:14 +0200

    prettier

--- lbcsClient/App.tsx
+++ lbcsClient/App.tsx
@@ -1,109 +1,155 @@
-import React, { useState, useMemo, useEffect, useCallback } from 'react';
-import { Text, View, StyleSheet, TouchableHighlight } from 'react-native';
-import Constants from 'expo-constants';
-import LBCSApi from './api';
-import FlashMessage, { showMessage } from 'react-native-flash-message';
-import * as R from 'ramda';
+import React, { useState, useMemo, useEffect, useCallback } from "react";
+import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
+import Constants from "expo-constants";
+import LBCSApi from "./api";
+import FlashMessage, { showMessage } from "react-native-flash-message";
+import * as R from "ramda";
 
 // or any pure javascript modules available in npm
-import { Avatar, Appbar, Menu, TextInput } from 'react-native-paper';
+import { Avatar, Appbar, Menu, TextInput } from "react-native-paper";
 
 function Cell({ ledNumber, selected, handleToggle }) {
-  const cellStyle ={
-      flex: 1,
-      backgroundColor: selected ? "blue" : "beige",
-      borderColor: "black", 
-      borderWidth: 1, 
-      borderRadius: 5
-    }
-  
-  return <TouchableHighlight style={cellStyle} onPress={() => handleToggle(ledNumber)} ><Text>{selected ? "ON" : "OFF"}</Text></TouchableHighlight>
+  const cellStyle = {
+    flex: 1,
+    backgroundColor: selected ? "blue" : "beige",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5
+  };
+
+  return (
+    <TouchableHighlight
+      style={cellStyle}
+      onPress={() => handleToggle(ledNumber)}
+    >
+      <Text>{selected ? "ON" : "OFF"}</Text>
+    </TouchableHighlight>
+  );
 }
 
 function Row({ cols, startIndex, gridState, handleToggle }) {
-  const cells = Array(cols).fill(null).map((_, i) => <Cell columns={cols} ledNumber={startIndex + i} key={i} selected={gridState[startIndex + i]} handleToggle={handleToggle} />)  
+  const cells = Array(cols)
+    .fill(null)
+    .map((_, i) => (
+      <Cell
+        columns={cols}
+        ledNumber={startIndex + i}
+        key={i}
+        selected={gridState[startIndex + i]}
+        handleToggle={handleToggle}
+      />
+    ));
 
-  return <View style={styles.row}>{cells}</View>
+  return <View style={styles.row}>{cells}</View>;
 }
 
 function Grid({ rows, cols, handleToggle, gridState }) {
-  const rowComponents = Array(rows).fill(null).map((_, i) => <Row cols={cols} startIndex={cols * i} key={i} gridState={gridState} handleToggle={handleToggle} />)  
-  return <View style={styles.container}>{rowComponents}</View>
+  const rowComponents = Array(rows)
+    .fill(null)
+    .map((_, i) => (
+      <Row
+        cols={cols}
+        startIndex={cols * i}
+        key={i}
+        gridState={gridState}
+        handleToggle={handleToggle}
+      />
+    ));
+  return <View style={styles.container}>{rowComponents}</View>;
 }
 
-
 export default function App() {
-  const [menuVisible, setMenuVisible] = useState(false)
-  const [gridState, setGridState] = useState({})
-  const [rows, setRows] = useState(0)
-  const [cols, setCols] = useState(0)
-  const [serverUrl, setServerUrl] = useState("http://192.168.2.18:8888/")
-  const [lbcsApi, setApi] = useState(new LBCSApi("http://192.168.2.18:8888/"))
-  const [serverUrlText, setServerUrlText] = useState("http://192.168.2.18:8888/")
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [gridState, setGridState] = useState({});
+  const [rows, setRows] = useState(0);
+  const [cols, setCols] = useState(0);
+  const [serverUrl, setServerUrl] = useState("http://192.168.2.18:8888/");
+  const [lbcsApi, setApi] = useState(new LBCSApi("http://192.168.2.18:8888/"));
+  const [serverUrlText, setServerUrlText] = useState(
+    "http://192.168.2.18:8888/"
+  );
 
   const syncWithServer = async () => {
     /* Load grid state and dimensions */
-    const response = await lbcsApi.state()
+    const response = await lbcsApi.state();
     if (!response.ok) {
-        showMessage({ message: "Something went wrong while getting grid from server", type: "danger"})
+      showMessage({
+        message: "Something went wrong while getting grid from server",
+        type: "danger"
+      });
     } else {
-        const data = await response.json()
-        setGridState(data)
-        setRows(data.rows)
-        setCols(data.columns)
+      const data = await response.json();
+      setGridState(data);
+      setRows(data.rows);
+      setCols(data.columns);
     }
-  }
+  };
 
   useEffect(() => {
     syncWithServer();
-  }, [lbcsApi])
+  }, [lbcsApi]);
 
   const handleToggle = (ledNumber: number) => {
-    setGridState((prevState) => {
-      const newState = {...prevState}
-      const value = newState[ledNumber]
+    setGridState(prevState => {
+      const newState = { ...prevState };
+      const value = newState[ledNumber];
 
       lbcsApi.setLed(ledNumber, !value).then(response => {
         if (!response.ok) {
-          showMessage({message: `Something went wrong while toggling ${ledNumber}`, type: "danger"})
-        } 
-      })
-      newState[ledNumber] = !value
-      return newState
-    })
-  }
+          showMessage({
+            message: `Something went wrong while toggling ${ledNumber}`,
+            type: "danger"
+          });
+        }
+      });
+      newState[ledNumber] = !value;
+      return newState;
+    });
+  };
 
-  const handleServerUrl = useCallback((newServerUrl) => {
-    setServerUrlText(newServerUrl)
-    try {
-      new URL(newServerUrl)
-    } catch(e) {
-      return;
-    }
+  const handleServerUrl = useCallback(
+    newServerUrl => {
+      setServerUrlText(newServerUrl);
+      try {
+        new URL(newServerUrl);
+      } catch (e) {
+        return;
+      }
 
-    if (newServerUrl === serverUrl) {
-      return;
-    }
+      if (newServerUrl === serverUrl) {
+        return;
+      }
 
-    setApi(new LBCSApi(newServerUrl))
-    setServerUrl(newServerUrl)
-
-  }, [setServerUrl, setApi, setServerUrlText, serverUrl])
+      setApi(new LBCSApi(newServerUrl));
+      setServerUrl(newServerUrl);
+    },
+    [setServerUrl, setApi, setServerUrlText, serverUrl]
+  );
 
   return (
     <View style={styles.container}>
       <Appbar.Header dark={true}>
-        <Avatar.Image size={48} source={"https://cdn3.iconfinder.com/data/icons/animals-105/150/icon_animal_touro-128.png"}/>
-        <Appbar.Content title="Little Bull Climbing System"/> 
+        <Avatar.Image
+          size={48}
+          source={
+            "https://cdn3.iconfinder.com/data/icons/animals-105/150/icon_animal_touro-128.png"
+          }
+        />
+        <Appbar.Content title="Little Bull Climbing System" />
         <Appbar.Action icon="sync" onPress={syncWithServer} />
         <Appbar.Action icon="dots-vertical" onPress={() => null} size={32} />
         <TextInput
-          label='Server'
+          label="Server"
           value={serverUrlText}
           onChangeText={handleServerUrl}
         />
       </Appbar.Header>
-      <Grid rows={rows} cols={cols} gridState={gridState} handleToggle={handleToggle} />
+      <Grid
+        rows={rows}
+        cols={cols}
+        gridState={gridState}
+        handleToggle={handleToggle}
+      />
       <FlashMessage position={"top"} />
     </View>
   );
@@ -112,16 +158,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
+    flexDirection: "column",
+    justifyContent: "flex-start",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
   row: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'stretch'
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "stretch"
   }
 });
--- lbcsClient/api.tsx
+++ lbcsClient/api.tsx
@@ -1,37 +1,26 @@
 class LBCSApi {
   constructor(baseUrl: string): string {
-    this.baseUrl = new URL(baseUrl)
+    this.baseUrl = new URL(baseUrl);
   }
 
   async canConnect() {
-    const response = await fetch(
-      new URL(`alive/`, this.baseUrl),
-    )
-    return response.ok
+    const response = await fetch(new URL(`alive/`, this.baseUrl));
+    return response.ok;
   }
 
-
   setLed(ledNumber: number, on: boolean): Promise {
-    return fetch(
-      new URL(`state/${ledNumber}/${on ? 1 : 0}/`, this.baseUrl),
-      {method: 'POST'}
-    )
+    return fetch(new URL(`state/${ledNumber}/${on ? 1 : 0}/`, this.baseUrl), {
+      method: "POST"
+    });
   }
 
   getDimensions(): Promise {
-    return fetch(
-      new URL(`dimensions/`, this.baseUrl),
-    )
+    return fetch(new URL(`dimensions/`, this.baseUrl));
   }
-  
+
   state(): Promise {
-    return fetch(
-      new URL(`state/`, this.baseUrl),
-      {mode: "cors"}
-    )
+    return fetch(new URL(`state/`, this.baseUrl), { mode: "cors" });
   }
-
 }
-
 
 export default LBCSApi;
