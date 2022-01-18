883ce5d97511ee8f4b6d654c4f23db6986ef8524
DonQueso89 <kg.v.ekeren@gmail.com>
2022-01-18 21:33:03 +0100

    install deps into image

--- backend/.boot.sh
+++ backend/.boot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# deps have been installed into the image but we run this
+# so that we dont need to rebuild it if we add a dependency
+# during development
 pip install -r requirements.txt
 python /srv/app/manage.py migrate
 python /srv/app/manage.py createsuperuser --noinput
